5daf9e50fc844dbd3c4c47f558f75fdf5c3297dc
simp <savarimani6@gmail.com>
2025-12-03 21:44:03 +0330

    User can now interact with Eva through chat

--- app/src/components/Chat/index.tsx
+++ app/src/components/Chat/index.tsx
@@ -1,37 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import BasicInput from "../../ui/inputs/BasicInput";
-import { AnimatePresence } from "motion/react";
+import { AnimatePresence, animate } from "motion/react";
 import { ChatInterface, SenderEnum } from "../../types/types";
 import ChatBubble from "../../ui/chatBubble";
 
 export default function Chat() {
   const [chats, setChats] = useState<ChatInterface | null>(null);
   const [input, setInput] = useState<string | null>(null);
+  const scrollRef = useRef(null);
 
   const loadChats = async () => {
     const chats = await window.eva.readChats();
 
-    return chats;
+    setChats(chats);
   };
 
   const sendMessage = async () => {
-    // TODO: Implement
+    const data = await window.eva.saveChats(SenderEnum.USER, input);
+    if (data.message === 200) loadChats();
   };
 
+  // Load chat and assign eva reponse callback
   useEffect(() => {
-    loadChats().then((chats) => setChats(chats));
+    loadChats();
+    window.eva.onMessageReady(() => loadChats());
   }, []);
+
+  // Animate smooth scroll
+  useEffect(() => {
+    const el = scrollRef.current;
+    if (!el) return;
+
+    animate(el.scrollTop, el.scrollHeight, {
+      duration: 0.4,
+      ease: "easeIn",
+      onUpdate: (value) => (el.scrollTop = value),
+    });
+  }, [chats]);
 
   return (
     <div className="p-5 flex w-[90%] h-[90%] bg-(--background-color) rounded-2xl flex-col">
-      <div className="flex-1">
+      <div className="flex-1 overflow-y-scroll scrollbar-hide" ref={scrollRef}>
         <AnimatePresence>
           {chats ? (
             chats.chats.map((items) => (
               <div
-                className={`flex ${items.sender == SenderEnum.USER ? "justify-end" : "justify-start"}`}
+                key={items.id}
+                className={`flex m-3 ${items.sender == SenderEnum.USER ? "justify-end" : "justify-start"}`}
               >
-                <ChatBubble sender={items.sender} text={items.message} key={items.id} />
+                <ChatBubble sender={items.sender} text={items.message} />
               </div>
             ))
           ) : (
